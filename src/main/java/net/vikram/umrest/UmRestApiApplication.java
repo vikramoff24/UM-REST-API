3a57305c58e07b7b1a3330ad3140ee45823044f1
Vikram S <vikram.senthil@outlook.com>
2020-11-05 23:21:09 +0530

    Intial Commit

---
+++ src/main/java/net/vikram/umrest/UmRestApiApplication.java
@@ -0,0 +1,13 @@
+package net.vikram.umrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmRestApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/net/vikram/umrest/entity/User.java
@@ -0,0 +1,5 @@
+package net.vikram.umrest.entity;
+
+public class User {
+
+}
